b40bde97f32d6838cffbcdc622341096af253453
William Chen <williamchenjian@gmail.com>
2023-10-12 11:49:11 -0400

    add move type and functions

---
+++ game/move.go
@@ -0,0 +1,21 @@
+package game
+
+// uses 16 bits to represent moves
+// AAABBBBBBCCCCCC
+// A - 3 bits for promotin
+// B - 6 bits for origin
+// C - 6 bits for dest
+
+type move uint16
+
+func (m *move) dest() int{
+	return int(*m & 0x3F)
+}
+
+func (m *move) origin() int{
+	return int((*m & 0xFC0) >> 6)
+}
+
+func (m *move) promotion() int{
+	return int((*m & 0x7000) >> 12)
+}
